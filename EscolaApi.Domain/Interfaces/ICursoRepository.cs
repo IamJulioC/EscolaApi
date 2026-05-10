bac10ac3e633d64d9b14e349c6d5325f771f1874
Júlio <iamjulioc@live.com>
2026-05-09 23:54:45 -0300

    first commit

---
+++ EscolaApi.Domain/Interfaces/ICursoRepository.cs
@@ -0,0 +1,16 @@
+﻿using EscolaApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EscolaApi.Domain.Interfaces
+{
+    internal interface ICursoRepository
+    {
+        Task<Curso> GetByIdAsync(int id);
+        Task<List<Curso>> GetAllAsync();
+        Task<Curso> AddAsync(Curso curso);
+        Task<Curso> UpdateAsync(Curso curso);
+        Task<Curso> DeleteAsync(int id);
+    }
+}
